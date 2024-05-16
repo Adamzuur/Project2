92dae0e6f1c6d05735f9f9e1f95a9f5d3e2f1429
adamz <adam.zuurveld@gmail.com>
2024-05-16 14:23:47 +0200

    Gebruiker class gemaakt

---
+++ src/main/java/com/example/project/Gebruiker.java
@@ -0,0 +1,48 @@
+package com.example.project;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Gebruiker {
+    private String gebruikersnaam;
+    private String wachtwoord;
+    private String email;
+    private final String AICHAT_BESTAND;
+
+    public String getWachtwoord() {
+        return wachtwoord;
+    }
+    public String getGebruikersnaam() {
+        return gebruikersnaam;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setGebruikersnaam(String gebruikersnaam) {
+        this.gebruikersnaam = gebruikersnaam;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
+    }
+    public Gebruiker(String gebruikersnaam, String wachtwoord, String email, String AICHAT_BESTAND) {
+        this.gebruikersnaam = gebruikersnaam;
+        this.wachtwoord = wachtwoord;
+        this.email = email;
+        this.AICHAT_BESTAND = gebruikersnaam + "_AICHAT.csv";
+        initBudgetBestand();
+    }
+    private void initBudgetBestand() {
+        File budgetBestand = new File(this.AICHAT_BESTAND);
+        if (!budgetBestand.exists()) {
+            try {
+                budgetBestand.createNewFile();
+            } catch (IOException var3) {
+                String var10001 = this.gebruikersnaam;
+                System.out.println("Fout bij het aanmaken van het budgetbestand voor gebruiker " + var10001 + ": " + var3.getMessage());
+            }
+        }
+    }
+}
--- src/main/java/com/example/project/HelloController.java
+++ src/main/java/com/example/project/HelloController.java
@@ -10,30 +10,15 @@ import javafx.scene.layout.AnchorPane;
 public class HelloController {
     @FXML
     private AnchorPane anchorPane;
-
     @FXML
     private Button loginButton;
-
     @FXML
     private Button registrerenButton;
-
     @FXML
     private PasswordField passwordField;
-
     @FXML
     private TextField gebruikersnaamTextfield;
-
     @FXML
     private Label foutmeldingLabel;
 
-    public void initialize() {
-        // Set the preferred size of the AnchorPane
-        anchorPane.setPrefSize(800, 600);
-
-        // Set the maximum size of the AnchorPane
-        anchorPane.setMaxSize(800, 600);
-
-        // Set the minimum size of the AnchorPane
-        anchorPane.setMinSize(800, 600);
-    }
 }
