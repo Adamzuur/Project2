866dee79b630314ca96c5f8580e3904ae7b4c736
adamz <adam.zuurveld@gmail.com>
2024-05-16 13:51:16 +0200

    Beginscherm, aanmelden class aangemaakt

--- src/main/java/com/example/project/HelloController.java
+++ src/main/java/com/example/project/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private AnchorPane anchorPane;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button loginButton;
+
+    @FXML
+    private Button registrerenButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField gebruikersnaamTextfield;
+
+    @FXML
+    private Label foutmeldingLabel;
+
+    public void initialize() {
+        // Set the preferred size of the AnchorPane
+        anchorPane.setPrefSize(800, 600);
+
+        // Set the maximum size of the AnchorPane
+        anchorPane.setMaxSize(800, 600);
+
+        // Set the minimum size of the AnchorPane
+        anchorPane.setMinSize(800, 600);
     }
 }
