9faec39c61723cc45a0ee966dead07e19f0722ea
Jaouadjj <165282739+Jaouadjj@users.noreply.github.com>
2024-05-23 15:10:04 +0200

    gh

--- src/main/java/com/example/project/Vragen.java
+++ src/main/java/com/example/project/Vragen.java
@@ -1,13 +1,25 @@
 package com.example.project;
+import java.util.Scanner;
 
-public class Vragen {
+
+public class Vragen implements ISendMessage {
+    Scanner scanner = new Scanner(System.in);
 
     private String vragen;
     private Gebruiker gebruiker;
 
-    public void vragenStellen(){
+    public void vragenStellen() {
+        System.out.println("Wat is uw onderwerp?");
+        String onderwerp = scanner.nextLine();
 
+        while (true) {
+            System.out.println("Stel uw vraag (of typ 'stop' om te stoppen):");
+            vragen = scanner.nextLine();
+
+            if (vragen.equalsIgnoreCase("stop")) {
+                break;
+            }
+        }
     }
-
 
 }
