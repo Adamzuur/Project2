ceab5bbd6b117836cd52ead2191f6be866e0cb58
Jaouadjj <165282739+Jaouadjj@users.noreply.github.com>
2024-05-24 09:37:13 +0200

    gh

--- src/main/java/com/example/project/Gesprek.java
+++ src/main/java/com/example/project/Gesprek.java
@@ -3,6 +3,7 @@ package com.example.project;
 public class Gesprek {
     private int gesprekID;
     private String onderwerp;
+    private Gebruiker gebruiker;
 
     public void stelVraag() {
 
--- src/main/java/com/example/project/Vragen.java
+++ src/main/java/com/example/project/Vragen.java
@@ -6,14 +6,13 @@ public class Vragen implements ISendMessage {
     Scanner scanner = new Scanner(System.in);
 
     private String vragen;
-    private Gebruiker gebruiker;
 
     public void vragenStellen() {
-        System.out.println("Wat is uw onderwerp?");
+        // System.out.println("Wat is uw onderwerp?");
         String onderwerp = scanner.nextLine();
 
         while (true) {
-            System.out.println("Stel uw vraag (of typ 'stop' om te stoppen):");
+            // System.out.println("Stel uw vraag (of typ 'stop' om te stoppen):");
             vragen = scanner.nextLine();
 
             if (vragen.equalsIgnoreCase("stop")) {
